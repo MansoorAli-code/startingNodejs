94db4f8f8b388b0574640e130c61d4de4f86193c
MansoorAli-code <shaikmohammedmansoorali@gmail.com>
2024-02-27 12:30:18 +0530

    Created a server , runned it on port 4000 and console logged my  name.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 4000;
+
+const server = http.createServer((req,res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, my name is Mansoor');
+})
+
+server.listen(port,hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
